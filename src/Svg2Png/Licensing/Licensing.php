aea3eae22e98341bce2039d9a0d3e2894d826310
Lukas Gaechter <lukas.gaechter@mind.ch>
2017-01-02 10:20:02 +0100

    Update EDD Plugin Updater

--- src/Svg2Png/Licensing/Licensing.php
+++ src/Svg2Png/Licensing/Licensing.php
@@ -8,14 +8,15 @@ namespace Svg2Png\Licensing;
  * Handles automatic plugin updates by registering with an external store.
  * In order for the free license to work, the following steps have to be conducted once:
  *
- * - The license key, which is the plugin slug has to be set in the download post
- *   of the store under Licensing > License Keys.
- * - Then the plugin needs to be downloaded once from the store defined
- *   in $store_url.
- * - Then the License needs to be activated on the store under Downloads > Licenses.
+ * 1. The license key (which is the plugin slug) has to be set in the download post
+ *    of the store under Licensing > License Keys.
+ * 2. The plugin needs to be downloaded once from the store defined in $store_url.
+ * 3. The License needs to be activated in the store under Downloads > Licenses.
+ *
+ * The license for the plugin is activated automatically when new plugin data is fetched.
  *
  * @author Lukas Gaechter
- * @version 1.0.0
+ * @version 1.1.1
  */
 class Licensing {
 	public $slug;
@@ -38,11 +39,19 @@ class Licensing {
 	public $store_url;
 
 	/**
-	 * Public license key.
+	 * Free license key.
 	 *
 	 * @var string
 	 */
 	private $_license_key;
+
+	/**
+	 * Name of key to use for cache transient
+	 *
+	 * @since 1.1.0
+	 * @var string
+	 */
+	private $_license_cache_key;
 
 	/**
 	 * Licensing constructor.
@@ -52,23 +61,17 @@ class Licensing {
 	 * @param string    $store_url      The URL to the store where Easy Digital Downloads is installed.
 	 */
 	public function __construct( $slug, $plugin_file, $store_url = 'https://products.mind.ch' ) {
-		if ( ! class_exists( 'EDD_SL_Plugin_Updater' ) ) {
-			require_once( __DIR__ . '/EDD_SL_Plugin_Updater.php' );
-		}
-
 		$this->slug = $slug;
 		$this->plugin_file = $plugin_file;
 		$this->store_url = $store_url;
 
 		$this->_license_key = $slug;
-
-		$plugin_data = get_plugin_data( $this->plugin_file, false, false );
-
-		$this->plugin_name = $plugin_data['Name'];
-		$this->plugin_version = $plugin_data['Version'];
-		$this->plugin_author = $plugin_data['Author'];
+		$this->_license_cache_key = $slug . '_license';
 
 		add_action( 'admin_init', [ $this, 'init' ] );
+
+		// Update license when new plugin data is fetched
+		add_filter( 'pre_set_site_transient_update_plugins', [ $this, 'maybe_activate_license' ], 11 );
 	}
 
 	/**
@@ -77,8 +80,14 @@ class Licensing {
 	 * @since 1.0.0
 	 */
 	public function init() {
+		$plugin_data = get_plugin_data( $this->plugin_file, false, false );
+
+		$this->plugin_name = $plugin_data['Name'];
+		$this->plugin_version = $plugin_data['Version'];
+		$this->plugin_author = $plugin_data['Author'];
+
 		// Setup the updater
-		new \EDD_SL_Plugin_Updater(
+		new EDD_SL_Plugin_Updater(
 			$this->store_url,
 			$this->plugin_file,
 			[
@@ -91,32 +100,96 @@ class Licensing {
 	}
 
 	/**
-	 * Activate the license for this site on the store URL.
+	 * Check with transient if plugin was activated before.
 	 *
-	 * You need to call this function from the activation hook of this plugin, e.g. like this:
+	 * @since 1.1.0
 	 *
-	 * register_activation_hook( __FILE__, function() {
-	 *     require_once( __DIR__ . '/vendor/autoload.php' );
+	 * @param $transient_data
+	 * @return mixed
+	 */
+	public function maybe_activate_license( $transient_data ) {
+		// Bailout if there’s no data for our plugin
+		if ( ! is_object( $transient_data )
+		     || empty( $transient_data->response )
+		     || empty( $transient_data->response[ plugin_basename( $this->plugin_file ) ] )
+		) {
+			return $transient_data;
+		}
+
+		$license = get_transient( $this->_license_cache_key );
+
+		if ( ! $license ) {
+			$this->activate_free_license();
+		}
+
+		return $transient_data;
+	}
+
+	/**
+	 * Activate the license for this site on the store.
 	 *
-	 *     $licensing = new Licensing( 'svg2png', plugin_dir_path( __FILE__ ) . 'svg2png' );
-	 *     $licensing->activate_free_license();
-	 * } );
+	 * Sets a 30-day transient that will save the license key. As long as this key is set,
+	 * the license will not be activated remotely.
 	 *
 	 * @since 1.0.0
 	 */
 	public function activate_free_license() {
-		// Data to send in our API request
-		$api_params = array(
+		// Data to send in API request
+		$api_params = [
 			'edd_action' => 'activate_license',
 			'license'    => $this->_license_key,
 			'item_name'  => urlencode( $this->plugin_name ),
 			'url'        => home_url(),
+		];
+
+		// Execute request
+		$response = wp_remote_post(
+			$this->store_url,
+			[ 'timeout' => 15, 'sslverify' => false, 'body' => $api_params ]
 		);
+
+		$license_data = json_decode( wp_remote_retrieve_body( $response ) );
+
+		if ( 'valid' === $license_data->license ) {
+			// Set transient whith license key which is valid for 30 days
+			set_transient( $this->_license_cache_key, $this->_license_key, 30 * 60 * 60 * 24 );
+
+			return true;
+		}
+
+		return false;
+	}
+
+	/**
+	 * Deactivate the license for this site on the store.
+	 *
+	 * You need to call this function from the deactivation hook of this plugin,
+	 * e.g. in your main plugin file:
+	 *
+	 * register_deactivation_hook( __FILE__, function() {
+	 *     require_once( __DIR__ . '/vendor/autoload.php' );
+	 *
+	 *     $licensing = new Licensing( 'your-plugin-slug', __FILE__ );
+	 *     $licensing->deactivate_free_license();
+	 * } );
+	 *
+	 * @since 1.1.0
+	 */
+	public function deactivate_free_license() {
+		// Data to send in API request
+		$api_params = [
+			'edd_action' => 'deactivate_license',
+			'license' => $this->_license_key,
+			'item_name' => urlencode( $this->plugin_name ),
+			'url' => home_url(),
+		];
 
 		// Execute request
 		wp_remote_post(
 			$this->store_url,
 			[ 'timeout' => 15, 'sslverify' => false, 'body' => $api_params ]
 		);
+
+		delete_transient( $this->_license_cache_key );
 	}
 }
