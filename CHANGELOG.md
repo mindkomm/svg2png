e12e424ecb5fabfa654356742bc9fb904b963a94
Lukas Gaechter <lukas.gaechter@mind.ch>
2017-01-02 10:22:25 +0100

    Update plugin version and Changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,9 +1,9 @@
 # Changelog
 
-## 1.0.1 - 2016-12-07
+## 1.0.1 - 2017-01-02
 
-* Fixed publish task
-* Filled Changelog
+* Updated plugin updater
+* Added optimized autoload files
 
 ## 1.0.0 - 2016-12-07
 
--- svg2png.php
+++ svg2png.php
@@ -9,7 +9,7 @@
  * Author URI: https://www.mind.ch
  * License: GNU General Public License v2.0 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
- * Version: 1.0.0
+ * Version: 1.0.1
  */
 namespace Svg2Png;
 
